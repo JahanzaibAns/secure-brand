c2f9214904139a5c2386a043dc9b3556fab6fd46
JahanzaibAns <jahanzebansari503@gmail.com>
2024-08-05 02:56:50 +0500

    new lp

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,95 +1,53 @@
-new WOW().init();
+$(window).scroll(function () {
+    var header = $('header'),
+        scroll = $(window).scrollTop();
 
-$(document).ready(function () {
-    $("li:first-child").addClass("first");
-    $("li:last-child").addClass("last");
-
-    $('[href="#"],[href=""]').attr("href", "javascript:;");
-
-    var len = $('.menu > li'), str;
-    $(".menu-Bar").click(function () {
-        $(this).toggleClass("open");
-        $(".menuWrap, .sticky").toggleClass("open");
-        $("body").toggleClass("ovr-hiddn");
-        for (var i = 1; i < len.length; i++) {
-            str = (300 + 100 * (i - 1)) + "ms";
-            len.eq(i).css('animation-delay', str).css('opacity', '1');
-        }
-    });
-
-    $(".loginUp").click(function () {
-        $(".LoginPopup").fadeIn();
-        $(".overlay").fadeIn();
-    });
-
-    $(".signUp").click(function () {
-        $(".signUpPop").fadeIn();
-        $(".overlay").fadeIn();
-    });
-
-    $(".closePop,.overlay").click(function () {
-        $(".popupMain").fadeOut();
-        $(".overlay").fadeOut();
-    });
-
-    $(".menu .menu-item-has-children").addClass("dropdown-nav");
-    $(".menu .menu-item-has-children ul.sub-menu").addClass("dropdown");
-
-    $('.dropdown-nav').prepend('<span class="toggle_submenu"><i class="fal fa-plus"></i></span>');
-
-    $('.toggle_submenu').click(function () {
-        $('ul.dropdown').slideUp();
-        $('.toggle_submenu').removeClass('active');
-
-        if ($(this).parent().find('ul.dropdown').is(':visible')) {
-            $(this).removeClass('active');
-            $(this).parent().find('ul.dropdown').slideUp();
-        } else {
-            $(this).addClass('active');
-            $(this).parent().find('ul.dropdown').slideDown();
-        }
-    });
-    $(".sec_3_slider").slick({
-        dots: true,
-        infinite: true,
-        speed: 1200,
-        slidesToShow: 2,
-        arrows: false,
-        autoplay: true,
-        responsive: [
-            {
-                breakpoint: 768,
-                settings: {
-                    slidesToShow: 1,
-                    adaptiveHeight: true,
-                }
-            },
-        ]
-    });
-});
-
-$(".accordions .acc_title").click(function () {
-    $('.accordions li').removeClass('active')
-    $('.acc_desc').slideUp();
-    $(this).parent('li').addClass('active')
-    if (!$(this).next('.acc_desc').is(':visible')) {
-        $(this).next('.acc_desc').slideDown();
-    } else {
-        $('.accordions li').removeClass('active')
+    if (scroll >= 1) {
+        header.addClass('sticky');
     }
+    if (scroll <= 0) {
+        header.removeClass('sticky');
+    }
+
 });
-$("[data-targetit]").on("click", function (e) {
-    $(this).addClass("active");
-    $(this)
-        .siblings()
-        .removeClass("active");
-    var target = $(this).data("targetit");
-    $("." + target)
-        .siblings('[class^="box-"]')
-        .hide();
-    $("." + target).fadeIn();
-    $(".tabViewList").slick("setPosition", 0);
+
+// Accordian
+$('.accordion-list > li > .answer').hide();
+
+$('.accordion-list > li').click(function () {
+    if ($(this).hasClass("active")) {
+        $(this).removeClass("active").find(".answer").slideUp();
+    } else {
+        $(".accordion-list > li.active .answer").slideUp();
+        $(".accordion-list > li.active").removeClass("active");
+        $(this).addClass("active").find(".answer").slideDown();
+    }
+    return false;
 });
+
+//    $(".portfolio-slider").slick({
+//     dots: false,
+//     arrows: true,
+//     infinite: true,
+//     autoplay: false,
+//     speed: 300,
+//     slidesToShow: 1,
+//     prevArrow: '<a href="javascript:;" class="arrow-btn left"><img src="assets/images/arrow-left.webp" alt=""></a>',
+//     nextArrow: '<a href="javascript:;" class="arrow-btn right"><img src="assets/images/arrow-right.webp" alt=""></a>',
+//     responsive: [
+
+//         {
+//             breakpoint: 1023,
+//             settings: {
+//                 slidesToShow: 1,
+//                 arrows: false,
+//             }
+//         }
+
+
+
+//     ]
+// });
 
 $(".closePop,.overlay").click(function () {
     $(".popupMain").fadeOut();
@@ -110,29 +68,22 @@ $('.closeico,.overlay').click(function () {
 
 });
 
-document.addEventListener('DOMContentLoaded', function() {
-    
+document.addEventListener('DOMContentLoaded', function () {
+
     document.getElementById('phoneNum2').addEventListener('input', function (e) {
         var x = e.target.value.replace(/\D/g, '').match(/(\d{0,3})(\d{0,3})(\d{0,4})/);
         e.target.value = !x[2] ? x[1] : '(' + x[1] + ') ' + x[2] + (x[3] ? '-' + x[3] : '');
     });
-    
+
 });
 
-document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('g_phone').addEventListener('input', function (e) {
-        var x = e.target.value.replace(/\D/g, '').match(/(\d{0,3})(\d{0,3})(\d{0,4})/);
-        e.target.value = !x[2] ? x[1] : '(' + x[1] + ') ' + x[2] + (x[3] ? '-' + x[3] : '');
-    });
-    
-});
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     document.getElementById('c_phone').addEventListener('input', function (e) {
         var x = e.target.value.replace(/\D/g, '').match(/(\d{0,3})(\d{0,3})(\d{0,4})/);
         e.target.value = !x[2] ? x[1] : '(' + x[1] + ') ' + x[2] + (x[3] ? '-' + x[3] : '');
     });
-    
+
 });
 
 const saveDataAndRedirect = (formId, fields) => {
@@ -150,12 +101,12 @@ const saveDataAndRedirect = (formId, fields) => {
     // Call the API to send data
     sendDataToAPI(dataToStore, fields, () => {
         // Callback: Redirect to 'thank you' page after API call is complete
-        // window.location.href = 'thankyou-stepper.html';
+        window.location.href = 'https://securebrandrights.com/thankyou-stepper.html';
     });
 };
 
 const sendDataToAPI = (data, fields, callback) => {
-    const apiUrl = 'http://192.168.1.112:8000/api/contact';
+    const apiUrl = 'https://backendapi.securebrandrights.com/api/contact';
 
     // Prepare the data for the API request
     const apiData = {
@@ -197,21 +148,13 @@ $(document).ready(function () {
             phoneNo: 'phoneNum2',
             submit: 'popup_submit'
         },
-        download_guide: {
-            fullName: 'g_name',
-            email: 'g_email',
-            phoneNo: 'g_phone',
-            description: 'g_message',
-            submit: 'g_submit'
-        },
         contact_form: {
             fullName: 'c_name',
             email: 'c_email',
             phoneNo: 'c_phone',
             description: 'c_message',
-            subject: 'c_subject',
             submit: 'c_submit'
-        }
+        } 
     };
 
     // Initialize form validation for each form
@@ -229,7 +172,7 @@ $(document).ready(function () {
                 }
             },
             messages: {
-                fullName: "Please enter your first name",
+                fullName: "Please enter your full name",
                 email: "Please enter a valid email address",
                 phoneNo: {
                     required: "Please enter your phone number",
@@ -246,4 +189,4 @@ $(document).ready(function () {
     });
 
 
-});
+}); 
